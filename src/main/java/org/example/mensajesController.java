046fc1e74d4953f968aba6b29e54a435768ac220
EstherGuzman07 <guzmansalazaresther1234@gmail.com>
2026-02-21 22:38:35 -0600

    clase de mensajes lista y conectada al de sugerencia

---
+++ src/main/java/org/example/mensajesController.java
@@ -0,0 +1,41 @@
+package org.example;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class mensajesController {
+
+    @FXML
+    void aceptarcadeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aceptarmikuButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inicioButtonPressed(ActionEvent event){
+
+
+
+    }
+
+    @FXML
+    void rechazarcadeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechazarmikuButtonPressed(ActionEvent event) {
+
+    }
+
+}
