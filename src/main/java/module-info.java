a679de3b933d441d28c1a3606cd157d2bf7c5369
jhonnyrg019 <jhonnyramirez052@gmail.com>
2026-02-21 22:57:19 -0600

    mensajes.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module sistema.citas {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.graphics;
+    requires javafx.base;
 
     opens org.example to javafx.fxml, javafx.controls, javafx.graphics;
     exports org.example;
--- src/main/java/org/example/mensajesController.java
+++ src/main/java/org/example/mensajesController.java
@@ -1,7 +1,14 @@
 package org.example;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 public class mensajesController {
 
     @FXML
@@ -15,7 +22,14 @@ public class mensajesController {
     }
 
     @FXML
-    void inicioButtonPressed(ActionEvent event) {
+    void inicioButtonPressed(ActionEvent event)throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/seugerencias.fxml")
+        );
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root));
+        stage.show();
 
     }
 
