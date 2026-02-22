30b48063f0f4f32fd41b20dc62b6d191404fcc85
EstherGuzman07 <guzmansalazaresther1234@gmail.com>
2026-02-21 21:05:50 -0600

    commit

---
+++ src/main/java/org/example/avisodeprivacidadController.java
@@ -0,0 +1,33 @@
+package org.example;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class avisodeprivacidadController {
+
+    @FXML
+    void aceptarprivacidadButtonPressed(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                avisodeprivacidadController.class.getResource("/sugerencias.fxml")
+        );
+
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+    @FXML
+    void rechazaraprivacidadButtonPressed(ActionEvent event) {
+
+    }
+
+}
