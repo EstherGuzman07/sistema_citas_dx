446eb7e31f9a61570fa25a7e12a7f07440a484c5
jhonnyrg019 <jhonnyramirez052@gmail.com>
2026-02-21 21:51:32 -0600

    agr mensajes.java

---
+++ src/main/java/org/example/Rechazocade.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Rechazocade {
+
+    @FXML
+    void aceptarmikuButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inicioButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechazarmikuButtonPressed(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/org/example/mensajesController.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+
+public class mensajesController {
+
+    @FXML
+    void aceptarcadeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aceptarmikuButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inicioButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechazarcadeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechazarmikuButtonPressed(ActionEvent event) {
+
+    }
+
+}
